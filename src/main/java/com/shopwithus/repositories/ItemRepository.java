7e9331e65f8dd0f7862c38425ebbe653f9f632a9
tanvi <tanvigupta910@gmail.com>
2021-09-03 21:31:45 -0400

    endpoint for crud item

--- src/main/java/com/shopwithus/repositories/ItemRepository.java
+++ src/main/java/com/shopwithus/repositories/ItemRepository.java
@@ -1,15 +1,12 @@
 package com.shopwithus.repositories;
 
-import java.util.List;
+import java.io.Serializable;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
 
 import com.shopwithus.models.Item;
 
-public interface ItemRepository extends JpaRepository<Item, Long> {
-	
-	@Query(value="SELECT * FROM items i where i.name = ?1",nativeQuery = true)
-	List<Item> findByName(String name);
+public interface ItemRepository extends CrudRepository<Item, Serializable> {
+		
 
 }
